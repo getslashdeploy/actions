8e3dd1a1c26f1dfe20ce415081f87bff5a513ca9
Tair Assimov <tair@assimov.net>
2019-01-12 19:53:50 +0100

    Fix path to bin

--- aws-eb/entrypoint.sh
+++ aws-eb/entrypoint.sh
@@ -4,13 +4,11 @@
 # Depends on https://github.com/unacast/actions/tree/master/github-deploy
 #
 
-BASEDIR=$(dirname "$0")
-
 # Respect AWS_DEFAULT_REGION if specified
 [ -n "$AWS_DEFAULT_REGION" ] || export AWS_DEFAULT_REGION=us-east-1
 
 # Pending
-"${BASEDIR}"/deployment-create-status pending
+${HOME}/bin/deployment-create-status pending
 
 # Execute
 output=$( sh -c "eb $*" )
@@ -19,7 +17,7 @@ output=$( sh -c "eb $*" )
 RESULT=$?
 if [ 0 != "${RESULT}" ]; then
   echo "Failed '$*'! Exit code '${RESULT}' is not equal to 0"
-  "${BASEDIR}"/deployment-create-status failure
+  ${HOME}/bin/deployment-create-status failure
   exit ${RESULT}
 fi
 
@@ -30,6 +28,6 @@ echo "$output" > "${HOME}/${GITHUB_ACTION}.${AWS_DEFAULT_OUTPUT}"
 echo "$output"
 
 # Success
-"${BASEDIR}"/deployment-create-status success
+${HOME}/bin/deployment-create-status success
 
 exit 0
